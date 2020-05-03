e95e24c4064d0851cd1b138fc4076f669418fe06
Antezh <Andreas_93_h@hotmail.se>
2020-05-03 11:22:24 +0200

    new

---
+++ Assets/Scripts/UIScaler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScaler : MonoBehaviour
+{
+    public float resoX;
+    public float resoY;
+
+    private CanvasScaler can;
+    // Start is called before the first frame update
+    void Start()
+    {
+        can = GetComponent<CanvasScaler>();
+        SetInfo();
+    }
+
+
+    void SetInfo()
+    {
+        resoX = (float)Screen.currentResolution.width;
+        resoY = (float)Screen.currentResolution.height;
+
+        can.referenceResolution = new Vector2(resoY, resoX);
+    }
+}
