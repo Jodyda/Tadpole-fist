6a8bc17aac0c7b52c0b53e16e955d8a26af65558
Sofia Fahlesson <sofiafahlesson@live.se>
2020-05-03 14:15:37 +0200

    Build settings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MovingObject
 
 	private Animator animator;
 	private int food;
-	//private Vector2 touchOrigin = -Vector2.one;
+	private Vector2 touchOrigin = -Vector2.one;
 
 
 
@@ -51,10 +51,10 @@ public class Player : MovingObject
         	return;
         }
 
-#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
-
         int horizontal = 0;
         int vertical = 0;
+
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
 
         horizontal = (int) Input.GetAxisRaw("Horizontal");
         vertical = (int) Input.GetAxisRaw("Vertical");
@@ -74,23 +74,23 @@ public class Player : MovingObject
 #else  
         if (Input.touchCount > 0) {
         	Touch myTouch = Input.touches[0];
-        }
 
-        if (myTouch.phase == TouchPhase.Began) {
-        	touchOrigin = myTouch.position;
-        }
-        else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
-        	Vector2 touchEnd = myTouch.position;
-        	float x = touchEnd.x - touchOrigin.x;
-        	float y = touchEnd.y - touchOrigin.y;
-        	touchOrigion.x = -1;
+            if (myTouch.phase == TouchPhase.Began) {
+            	touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+            	Vector2 touchEnd = myTouch.position;
+            	float x = touchEnd.x - touchOrigin.x;
+            	float y = touchEnd.y - touchOrigin.y;
+            	touchOrigin.x = -1;
 
-        	if (Mathf.Abs(x) > Mathf.Abs(y)) {
-        		horizontal = x > 0 ? 1 : -1;
-        	}
-        	else {
-        		vertical = y > 0 ? 1 : -1;
-        	}
+            	if (Mathf.Abs(x) > Mathf.Abs(y)) {
+            		horizontal = x > 0 ? 1 : -1;
+            	}
+            	else {
+            		vertical = y > 0 ? 1 : -1;
+            	}
+            }
         }
 
 #endif
