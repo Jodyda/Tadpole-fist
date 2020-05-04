4558a7ba3e47ff685a10c62f571e1712eea47c0f
Jodyda <63587334+Jodyda@users.noreply.github.com>
2020-05-04 14:58:23 +0200

    Tagit bort delay på animation samt fixat TadpoleDrop lite

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -18,6 +18,25 @@ public class PlayerAbilities : MonoBehaviour
 
     void DropItem()
     {
+<<<<<<< Updated upstream
         Instantiate(TadpoleDrop, this.gameObject.transform.position - transform.forward * 4, Quaternion.identity);
     }
 }
+=======
+
+       
+
+        if (TadpoleDrop)
+        {
+            Instantiate(TadpoleDrop, new Vector2(Mathf.RoundToInt(transform.position.x),
+            Mathf.RoundToInt(transform.position.y)),
+            TadpoleDrop.transform.rotation);
+        }
+
+    }
+}
+//Utbytt kod
+//Instantiate(TadpoleDrop, this.gameObject.transform.position - transform.forward * 4, Quaternion.identity);
+
+
+>>>>>>> Stashed changes
