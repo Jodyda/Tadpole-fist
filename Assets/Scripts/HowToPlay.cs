7994efee345756c9e1558e6beb31e2aa97e018aa
Sofia Fahlesson <sofiafahlesson@live.se>
2020-06-02 10:57:30 +0200

    Added HowToPlay

---
+++ Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlay : MonoBehaviour
+{
+
+	public static bool isHowToOpen = false;
+
+	public GameObject howToGUI;
+
+    public void openHowTo() {
+    	howToGUI.SetActive(true);
+    }
+
+    public void closeHowTo() {
+    	howToGUI.SetActive(false);
+    }
+}
